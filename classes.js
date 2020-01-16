5a8dd4f11d3feb575652ebbe1a39cfbbab638374
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-01-16 14:07:35 -0700

    adds solutions for classes doc

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget'
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,7 +58,18 @@
 */
 
 //Code Here
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(newEmployee){
+    this.reports.push(newEmployee)
+  }
+  fire(empIndex){
+    this.reports.splice(empIndex, 1)
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,8 +93,36 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
 
-
+  hire(){
+    super.hire()
+    let len = this.reports.length
+    if (len === 0){
+      this.title = 'Not a manager'
+    } else if (len > 0 && len <= 3){
+      this.title = 'Barely Manager'
+    } else if (len > 3 && len <= 10){
+      this.title = 'Mostly Manager'
+    } else if (len > 10 && len <= 50){
+      this.title = 'Manager'
+    } else if (len > 50 && len <= 100){
+      this.title = 'Manager Plus'
+    } else if (len > 100){
+      this.title = 'Bestest Manager'
+    } 
+  }
+  
+  fire(){
+    super.fire
+    this.bonus += 100
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -99,5 +148,27 @@
 */
 
 //Code Here
-
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num
+    let a = this.widgets_made_count
+    this.wear_and_tear_count = Math.floor(a / 50)
+  }
+  fixMachine(){
+    this.needs_reboot = true
+    // console.log(this.needs_reboot)
+  }
+  reboot(){
+    return function(){
+      this.needs_reboot = false
+      this.wear_and_tear_count -= 10
+      console.log(this.needs_reboot)
+    }
+  }
+}
 
